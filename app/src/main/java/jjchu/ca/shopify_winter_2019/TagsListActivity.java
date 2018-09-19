b99b89c70e06af570de5e11aa18eb6a1e7ba02d8
Jason Chu <jjchu@uwaterloo.ca>
2018-09-19 01:40:43 -0400

    Created barebones TagsListActivity.

--- app/src/main/java/jjchu/ca/shopify_winter_2019/TagsListActivity.java
+++ app/src/main/java/jjchu/ca/shopify_winter_2019/TagsListActivity.java
@@ -2,12 +2,28 @@ package jjchu.ca.shopify_winter_2019;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class TagsListActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tags_list);
+
+        recyclerView = findViewById(R.id.tags_recycler_view);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        //TODO: Create adapter
+        //adapter = new tagAdapter(tagData);
+        //recyclerView.setAdapter(adapter);
+
     }
 }
