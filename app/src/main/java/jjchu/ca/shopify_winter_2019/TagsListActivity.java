cf5bec5736c2a6c39e4214b0f82298c92af91968
Jason Chu <jjchu@uwaterloo.ca>
2018-09-19 01:17:32 -0400

    Created project.

---
+++ app/src/main/java/jjchu/ca/shopify_winter_2019/TagsListActivity.java
@@ -0,0 +1,13 @@
+package jjchu.ca.shopify_winter_2019;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TagsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tags_list);
+    }
+}
