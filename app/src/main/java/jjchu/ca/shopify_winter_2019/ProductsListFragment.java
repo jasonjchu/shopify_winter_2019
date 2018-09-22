c33c4872830edf9e083f942ee0403a94883730ef
Jason Chu <jjchu@uwaterloo.ca>
2018-09-22 13:14:24 -0400

    Optimized imports. Changed app name.

--- app/src/main/java/jjchu/ca/shopify_winter_2019/ProductsListFragment.java
+++ app/src/main/java/jjchu/ca/shopify_winter_2019/ProductsListFragment.java
@@ -5,13 +5,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
 
 import jjchu.ca.shopify_winter_2019.RecyclerAdapters.ProductsAdapter;
 
